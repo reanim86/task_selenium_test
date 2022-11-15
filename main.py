4e81b7a57302279d5223169129300ad9acadf3a3
Aleksei Kudinov <reanim86@yandex.ru>
2022-11-15 18:01:08 +0300

    No enter pass

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 driver.get('https://passport.yandex.ru/auth/')
 name = driver.find_element('id', 'passp-field-login')
 name.send_keys('reanim86')
+name_click = driver.find_element('id', 'passp:sign-in')
+name_click.click()
+wait.until(visibilityOfElementLocated(By.id("menu")))
+password = driver.find_element('id', 'passp-field-passwd')
+# password.send_keys('Tehn4321')
